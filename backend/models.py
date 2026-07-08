3b22cd435314169df2c0cf53e3e19b2232640535
St Michael Egenamba <michaelegenamba@gmail.com>
2026-07-08 14:52:54 -0700

    added initial models and relationships

--- backend/models.py
+++ backend/models.py
@@ -3,15 +3,47 @@ from sqlalchemy.orm import validates
 from sqlalchemy import CheckConstraint
 from datetime import datetime
 
-
 db = SQLAlchemy()
 
 class Habit(db.Model):
     __tablename__ = "habit"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False), 
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=True)
+    name = db.Column(db.String(100), nullable=False) 
     description = db.Column(db.Text)
-    isActive = db.Column(db.Bool, nullable=False)
-    createdAt = db.Column(db.DateTime, nullable=False)
-    updatedAt = db.Column(db.DateTime, nullable=False)
+    isActive = db.Column(db.Boolean, nullable=True)
+    createdAt = db.Column(db.DateTime, nullable=False, server_default=db.func.now())
+    updatedAt = db.Column(
+        db.DateTime, 
+        nullable=False, 
+        server_default=db.func.now(), 
+        onupdate=db.func.now()
+    )
+    sessions = db.relationship("Session", backref = 'habit')
+    goals = db.relationship("Goal", backref = 'habit')
+
+class Session(db.Model):
+    __tablename__ = 'session'
+
+    id = db.Column(db.Integer, primary_key=True)
+    habit_id = db.Column(db.Integer, db.ForeignKey('habit.id'), nullable=False)
+    start_time = db.Column(db.DateTime, nullable=False, server_default=db.func.now())
+    end_time = db.Column(db.DateTime, nullable=True)
+    duration = db.Column(db.Integer, nullable=True)
+
+class Category(db.Model):
+    __tablename__ = 'category'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+    habits = db.relationship("Habit", backref = 'category')
+
+class Goal(db.Model):
+    __tablename__ = 'goal'
+
+    id = db.Column(db.Integer, primary_key=True)
+    habit_id = db.Column(db.Integer, db.ForeignKey('habit.id'), nullable=False)
+    target_hours = db.Column(db.Integer, nullable=True)
+    deadline = db.Column(db.DateTime, nullable=True)
