483a9d62c91ee8ca5cbe03d2a26b71cee085a7d0
St Michael Egenamba <michaelegenamba@gmail.com>
2026-07-18 09:02:46 -0700

    Updating api folder

--- frontend/api/goals.js
+++ frontend/api/goals.js
@@ -4,21 +4,21 @@ axios.defaults.baseURL = "https://habitclock-1.onrender.com/";
 axios.defaults.withCredentials = true;
 
 export function getGoals() {
-  return axios.get("/goals");
+  return axios.get("/goals/");
 }
 
 export function getGoal(id) {
-  return axios.get(`/goals${id}`);
+  return axios.get(`/goals/${id}`);
 }
 
 export function createGoal(data) {
-  return axios.post("/goals", data);
+  return axios.post("/goals/", data);
 }
 
 export function updateGoal(id, data) {
-  return axios.patch(`/goals${id}`, data);
+  return axios.patch(`/goals/${id}`, data);
 }
 
 export function deleteGoal(id) {
-  return axios.delete(`/goals${id}`);
+  return axios.delete(`/goals/${id}`);
 }
