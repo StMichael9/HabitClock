07c916bbd38b14f6f19d8b28ec11f6381d2dec30
St Michael Egenamba <michaelegenamba@gmail.com>
2026-07-18 14:15:35 -0700

    Fixing bugs

--- frontend/api/sessions.js
+++ frontend/api/sessions.js
@@ -4,17 +4,17 @@ axios.defaults.baseURL = "https://habitclock-1.onrender.com/";
 axios.defaults.withCredentials = true;
 
 export function getSessions(habitId) {
-  return axios.get(`/sessions${habitId}`);
+  return axios.get(`/sessions/${habitId}`);
 }
 
 export function startSession(habitId) {
-  return axios.post(`/sessions${habitId}/start`);
+  return axios.post(`/sessions/${habitId}/start`);
 }
 
 export function stopSession(habitId) {
-  return axios.post(`/sessions${habitId}/stop`);
+  return axios.post(`/sessions/${habitId}/stop`);
 }
 
 export function deleteSession(sessionId) {
-  return axios.delete(`/sessions${sessionId}`);
+  return axios.delete(`/sessions/${sessionId}`);
 }
